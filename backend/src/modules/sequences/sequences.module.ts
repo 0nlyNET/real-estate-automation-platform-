4ffedb67dcc53749f404779c57d5bc543fc6eedd
Jayden King <jk@Jaydens-MacBook-Air.local>
2026-01-03 21:28:02 -0500

    Fix TypeORM metadata: register SequenceStep entity

--- backend/src/modules/sequences/sequences.module.ts
+++ backend/src/modules/sequences/sequences.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sequence } from './sequence.entity';
 import { SequenceEnrollment } from './sequence-enrollment.entity';
+import { SequenceStep } from './sequence-step.entity';
 import { SequencesService } from './sequences.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sequence, SequenceEnrollment])],
+  imports: [TypeOrmModule.forFeature([Sequence, SequenceEnrollment, SequenceStep])],
   providers: [SequencesService],
   exports: [SequencesService],
 })
