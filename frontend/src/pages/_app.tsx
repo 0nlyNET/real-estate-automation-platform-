9631a8244c4cded76052e4067be892dde0b24371
Jayden King <jk@Jaydens-MacBook-Air.local>
2026-01-04 23:09:20 -0500

    Finalize favicon setup

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "@styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
